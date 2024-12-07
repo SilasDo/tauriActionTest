5366fad218cd110470b428e704bd7bbfe608776b
Silas Dohm <silas.dohm@stud.hs-bochum.de>
2024-12-07 14:39:03 +0100

    update functionality added

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,36 @@
 <script setup lang="ts">
 import { ref } from "vue";
 import { invoke } from "@tauri-apps/api/core";
+import { check } from '@tauri-apps/plugin-updater';
+import { relaunch } from '@tauri-apps/plugin-process';
+
+const update = await check();
+if (update) {
+  console.log(
+    `found update ${update.version} from ${update.date} with notes ${update.body}`
+  );
+  let downloaded = 0;
+  let contentLength = 0;
+  // alternatively we could also call update.download() and update.install() separately
+  await update.downloadAndInstall((event) => {
+    switch (event.event) {
+      case 'Started':
+        contentLength = event.data.contentLength ??0;
+        console.log(`started downloading ${event.data.contentLength} bytes`);
+        break;
+      case 'Progress':
+        downloaded += event.data.chunkLength;
+        console.log(`downloaded ${downloaded} from ${contentLength}`);
+        break;
+      case 'Finished':
+        console.log('download finished');
+        break;
+    }
+  });
+
+  console.log('update installed');
+  await relaunch();
+}
 
 const greetMsg = ref("");
 const name = ref("");
@@ -13,7 +43,7 @@ async function greet() {
 
 <template>
   <main class="container">
-    <h1>Welcome to Tauri + Vue v 0.1.1</h1>
+    <h1>Welcome to Tauri + Vue v 0.1.0</h1>
 
     <div class="row">
       <a href="https://vitejs.dev" target="_blank">
