ad521ef6bc923f76373075eb14ae6de11c0dbd0a
Silas Dohm <silas.dohm@stud.hs-bochum.de>
2024-12-07 15:14:46 +0100

    0.1.1 patch

--- src/App.vue
+++ src/App.vue
@@ -50,7 +50,7 @@ async function greet() {
 
 <template>
   <main class="container">
-    <h1>Welcome to Tauri + Vue v 0.1.0</h1>
+    <h1>Welcome to Tauri + Vue v 0.1.1</h1>
 
     <div class="row">
       <a href="https://vitejs.dev" target="_blank">
