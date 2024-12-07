76357a6c86add7afcbd2f16ec3e23d58cd17e52a
Silas Dohm <silas.dohm@stud.hs-bochum.de>
2024-12-07 14:12:55 +0100

    0.1.1

--- src/App.vue
+++ src/App.vue
@@ -13,7 +13,7 @@ async function greet() {
 
 <template>
   <main class="container">
-    <h1>Welcome to Tauri + Vue</h1>
+    <h1>Welcome to Tauri + Vue v 0.1.1</h1>
 
     <div class="row">
       <a href="https://vitejs.dev" target="_blank">
