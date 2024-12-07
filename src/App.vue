d83b94122dc532377fc6d4060e0c7b23f633e47d
Silas Dohm <silas.dohm@stud.hs-bochum.de>
2024-12-07 15:03:50 +0100

    moved update to onmounted, target just linux for testing

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,43 @@
 <script setup lang="ts">
-import { ref } from "vue";
+import { ref, onMounted } from "vue";
 import { invoke } from "@tauri-apps/api/core";
 import { check } from '@tauri-apps/plugin-updater';
 import { relaunch } from '@tauri-apps/plugin-process';
 
-const update = await check();
-if (update) {
-  console.log(
-    `found update ${update.version} from ${update.date} with notes ${update.body}`
-  );
-  let downloaded = 0;
-  let contentLength = 0;
-  // alternatively we could also call update.download() and update.install() separately
-  await update.downloadAndInstall((event) => {
-    switch (event.event) {
-      case 'Started':
-        contentLength = event.data.contentLength ??0;
-        console.log(`started downloading ${event.data.contentLength} bytes`);
-        break;
-      case 'Progress':
-        downloaded += event.data.chunkLength;
-        console.log(`downloaded ${downloaded} from ${contentLength}`);
-        break;
-      case 'Finished':
-        console.log('download finished');
-        break;
-    }
-  });
 
-  console.log('update installed');
-  await relaunch();
+async function checkForUpdates() {
+  const update = await check();
+  if (update) {
+    console.log(
+      `found update ${update.version} from ${update.date} with notes ${update.body}`
+    );
+    let downloaded = 0;
+    let contentLength = 0;
+    await update.downloadAndInstall((event) => {
+      switch (event.event) {
+        case 'Started':
+          contentLength = event.data.contentLength ?? 0;
+          console.log(`started downloading ${event.data.contentLength} bytes`);
+          break;
+        case 'Progress':
+          downloaded += event.data.chunkLength;
+          console.log(`downloaded ${downloaded} from ${contentLength}`);
+          break;
+        case 'Finished':
+          console.log('download finished');
+          break;
+      }
+    });
+
+    console.log('update installed');
+    await relaunch();
+  }
 }
+
+onMounted(() => {
+  checkForUpdates();
+});
+
 
 const greetMsg = ref("");
 const name = ref("");
